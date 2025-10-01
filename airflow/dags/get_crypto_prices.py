d7aebd086bd319aa2bf6e87c67380c0e8e54f4c5
fsocha <01131759@pw.edu.pl>
2025-10-01 23:05:19 +0200

    Commit: Projekt ETL

---
+++ airflow/dags/get_crypto_prices.py
@@ -0,0 +1,11 @@
+import datetime
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="crypto_prices_dag",
+    start_date=datetime.datetime(2025, 2, 3),
+    schedule="@daily",
+):
+    EmptyOperator(task_id="task")
